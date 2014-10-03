df6bcdf9b1087b1ad782279a0a76258a67bcb0ba
ericdouglas <eric.eoop@gmail.com>
2014-10-03 15:57:17 -0300

    JavaScript: Math.round()

---
+++ javascript/Math/round.js
@@ -0,0 +1,14 @@
+// Math.round() : Arredonda para o inteiro mais próximo, para baixo ou para cima
+
+
+console.log( '13.01 => ' + Math.round(13.01) );
+console.log( '31.99 => ' + Math.round(31.99) );
+console.log( '"0" => ' + Math.round('0') );
+console.log( '"NaN" => ' + Math.round('NaN') );
+console.log( 'NaN => ' + Math.round(NaN) );
+console.log( '"456.21" => ' + Math.round('456.21') );
+console.log( '"13.84" => ' + Math.round('13.84') );
+console.log( '-31.13 => ' + Math.round(-31.13) );
+console.log( '31 => ' + Math.round(31) );
+
+// Dica: Arredonda números '.5' para cima. Ex: 5.5 => 6 
