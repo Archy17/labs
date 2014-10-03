a25c33c85497f42f2742ff1966ea57e5d4b81621
ericdouglas <eric.eoop@gmail.com>
2014-10-03 15:25:28 -0300

    JavaScript: Math.pow()

---
+++ javascript/Math/pow.js
@@ -0,0 +1,10 @@
+// Math.pow( x, y ) : Calcula a potência de X elevado a Y
+
+console.log( '2, 3 => ' + Math.pow( 2, 3 ) );
+console.log( '"3", 5 => ' + Math.pow( '3', 5 ) );
+console.log( '5, 0 => ' + Math.pow( 5, 0 ) );
+console.log( '3, "One" => ' + Math.pow( 3, 'One' ) );
+console.log( '-3, -1 => ' + Math.pow( -3, -1 ) );
+console.log( '3, -1 => ' + Math.pow( 3, -1 ) );
+console.log( '1.3, 3.1 => ' + Math.pow( 1.3, 3.1 ) );
+console.log( '-1.3, -3.1 => ' + Math.pow( -1.3, -3.1 ) );
