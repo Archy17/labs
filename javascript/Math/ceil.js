83a6b31c5322c201928af4464c9857d707817664
ericdouglas <eric.eoop@gmail.com>
2014-10-03 14:54:00 -0300

    JavaScript: Math.ceil()

---
+++ javascript/Math/ceil.js
@@ -0,0 +1,11 @@
+// Math.ceil() : Arredonda um número para cima
+
+console.log( '13.01 => ' + Math.ceil(13.01) );
+console.log( '31.99 => ' + Math.ceil(31.99) );
+console.log( '"0" => ' + Math.ceil('0') );
+console.log( '"NaN" => ' + Math.ceil('NaN') );
+console.log( 'NaN => ' + Math.ceil(NaN) );
+console.log( '"456.21" => ' + Math.ceil('456.21') );
+console.log( '"13.84" => ' + Math.ceil('13.84') );
+
+// Dica: Math.ceil() faz coerção de tipo
