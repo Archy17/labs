9f994ec329458cb777e755211d423dc1a1a8dc33
ericdouglas <eric.eoop@gmail.com>
2014-10-03 14:43:44 -0300

    JavaScript: Math.abs()

---
+++ javascript/Math/abs.js
@@ -0,0 +1,12 @@
+// Math.abs() : calcula um valor absoluto
+
+console.log( Math.abs( 13 ) );
+console.log( Math.abs( -31 ) );
+console.log( Math.abs( '13' ) );
+console.log( Math.abs( 'NaN' ) );
+console.log( Math.abs( 0 ) );
+console.log( Math.abs( 'zero'  ));
+
+console.log( typeof Math.abs( 'NaN' ) );
+
+// Dica: o método abs() faz coerção de tipo
