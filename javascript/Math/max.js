51a4f9ea33d3231fee34da51da8a0435997edad4
ericdouglas <eric.eoop@gmail.com>
2014-10-03 15:05:47 -0300

    JavaScript: Math.max()

---
+++ javascript/Math/max.js
@@ -0,0 +1,9 @@
+// Math.max() : retorna o maior argumento
+
+console.log( 'Args: 1, 4, 6, 8, 13, 2 => ' + Math.max( 1, 4, 6, 8, 13, 2 ) );
+console.log( 'Args: [ 1, 4, 6, 8, 13, 2 ] => ' + Math.max( [ 1, 4, 6, 8, 13, 2 ] ) );
+
+/*
+  Retorna o maior dos argumentos. Caso o argumento não possa ser convertido
+  em um valor númerico, retorna NaN
+*/
