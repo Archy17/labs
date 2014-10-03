54e99072ab9627cd0347f3aad431d0d904943dde
ericdouglas <eric.eoop@gmail.com>
2014-10-03 14:35:48 -0300

    JavaScript: Math constants

---
+++ javascript/Math/constants.js
@@ -0,0 +1,8 @@
+console.log( 'Math.E (base do logaritmo natural) : ' + Math.E );
+console.log( 'Math.LN10 (logaritmo natural de 10) : ' + Math.LN10 );
+console.log( 'Math.LN2 (logaritmo natural de 2) : ' + Math.LN2 );
+console.log( 'Math.LOG10E (logaritmo de base 10 de *e*) : ' + Math.LOG10E );
+console.log( 'Math.LOG2E (logaritmo de base 2 de *e*) : ' + Math.LOG2E );
+console.log( 'Math.PI (constante π) : ' + Math.PI );
+console.log( 'Math.SQRT1_2 (número 1 dividido pela raiz quadrada de 2) : ' + Math.SQRT1_2 );
+console.log( 'Math.SQRT2 (raiz quadrada de 2) : ' + Math.SQRT2 );
