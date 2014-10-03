96071ff8252138cac196eac6db0743a553c58f6f
ericdouglas <eric.eoop@gmail.com>
2014-10-03 16:00:25 -0300

    JavaScript: Math.sqrt()

---
+++ javascript/Math/sqrt.js
@@ -0,0 +1,11 @@
+// Math.sqrt() : Calcula a raiz quadrada de um número
+
+console.log( '13 => ' + Math.sqrt( 13 ) );
+console.log( '64 => ' + Math.sqrt( 64 ) );
+console.log( '81 => ' + Math.sqrt( 81 ) );
+console.log( '-81 => ' + Math.sqrt( -81 ) );
+console.log( '4 => ' + Math.sqrt( 4 ) );
+console.log( '16 => ' + Math.sqrt( 16 ) );
+console.log( '"-16" => ' + Math.sqrt( '-16' ) );
+console.log( '5.8 => ' + Math.sqrt( 5.8 ) );
+console.log( '"0" => ' + Math.sqrt( '0' ) );
