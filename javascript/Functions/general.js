9f0b7a27e5706b4cc015fcd5e276f96e642185ca
ericdouglas <eric.eoop@gmail.com>
2014-10-04 20:29:48 -0300

    JavaScript: function declaration VS function expression

--- javascript/Functions/general.js
+++ javascript/Functions/general.js
@@ -2,3 +2,17 @@
 function soma( a, b ) {
   return a + b;
 }
+
+// Expressão de função: atribuir uma função anônima a uma variável
+var multiplica = function( a, b ) {
+  return a * b;
+};
+
+/*
+  === Diferenças
+
+  Declarações de função são içadas para o topo do código,
+  sendo carregadas primeiro que qualquer outro código.
+  Você pode executar essas funções de qualquer local.
+
+*/
