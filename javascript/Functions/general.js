e8efec999dcdbdd81adfdc0331be6f502b2d4f4c
ericdouglas <eric.eoop@gmail.com>
2014-10-04 20:48:39 -0300

    JavaScript: object arguments

--- javascript/Functions/general.js
+++ javascript/Functions/general.js
@@ -16,3 +16,22 @@ var multiplica = function( a, b ) {
   Você pode executar essas funções de qualquer local.
 
 */
+
+// arguments : é um objeto 'array-like' que corresponde
+// aos argumentos passados para um função
+
+function imprimiValor( valor ) {
+  console.log( valor );
+  console.log( arguments.length );
+  console.log( arguments.callee ); // faz referência a função em execução
+  console.log( arguments.caller ); // faz referência a função que a chamou
+
+}
+
+function chamaFuncao( fn ) {
+  fn();
+}
+
+chamaFuncao( imprimiValor );
+
+// arguments.callee é muito bom para recursão!
