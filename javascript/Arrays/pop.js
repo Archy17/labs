b12e15cea7d6e043b43bf348cf9910b62bd46bba
ericdouglas <eric.eoop@gmail.com>
2014-10-06 08:40:12 -0300

    JavaScript: pop() method ~> Array

---
+++ javascript/Arrays/pop.js
@@ -0,0 +1,24 @@
+/*
+
+  Array.pop()
+
+  - Exclui o último elemento do array
+  - Retorna o valor excluído
+  ps: Se o array está vazio, não altera nada e retorna undefined
+
+*/
+
+var stack = [ 'JS', 'HTML', 'CSS' ];
+
+console.log( stack.pop() );
+console.log( stack );
+
+console.log( stack.pop() );
+console.log( stack );
+
+console.log( stack.pop() );
+console.log( stack );
+
+console.log( stack.pop() );
+console.log( stack );
+
