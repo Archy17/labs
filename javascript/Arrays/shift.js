d5b5825f70679066822f3643e9da8a6ea2177953
ericdouglas <eric.eoop@gmail.com>
2014-10-06 09:38:45 -0300

    JavaScript: shift() method ~> Array

---
+++ javascript/Arrays/shift.js
@@ -0,0 +1,23 @@
+/*
+
+  Array.shift()
+
+  - Remove o primeiro elemento do array e o retorna
+  - Caso o array esteja vazio, retorna undefined
+
+*/
+
+var colors = [ 'red', 'blue', 'green' ];
+
+console.log( colors.shift() );
+console.log( colors );
+
+
+console.log( colors.shift() );
+console.log( colors );
+
+console.log( colors.shift() );
+console.log( colors );
+
+console.log( colors.shift() );
+console.log( colors );
