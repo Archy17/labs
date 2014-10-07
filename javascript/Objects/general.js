3a83c5328b40531a0d16778fd61746a3d163cf83
ericdouglas <eric.eoop@gmail.com>
2014-10-07 11:26:52 -0300

    JavaScript: constructor function ~> Object

--- javascript/Objects/general.js
+++ javascript/Objects/general.js
@@ -1,4 +1,4 @@
-// Sintaxe Literal
+/********** Sintaxe Literal **********/
 var guitar = {
   brand: 'Music Man',
   model: 'Majesty',
@@ -9,7 +9,7 @@ var guitar = {
 
 console.log( guitar.fullName() );
 
-// Função Factory
+/********** Função Factory **********/
 
 function factoryGuitar( brand, model, price ) {
   return {
@@ -28,3 +28,40 @@ var ib = factoryGuitar( 'Ibanez', 'JEM777', 4050.99 );
 
 console.log( mm.getNameAndPrice() );
 console.log( ib.getNameAndPrice() );
+
+/********** Função Construtora **********/
+
+var Guitar = function( brand, model, price ) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+
+  this.getNameAndPrice = function() {
+    return this.brand + ' ' + this.model + ': $' + this.price;
+  };
+
+  this.comparePrice = function( otherGuitar ) {
+    if ( otherGuitar instanceof Guitar ) {
+      return 'The price difference between ' + this.brand + ' ' + this.model +
+        ' and ' + otherGuitar.brand + ' ' + otherGuitar.model + ' is: $' +
+        Math.abs( this.price - otherGuitar.price );
+    } else {
+      return "Sorry, we couldn't compare this";
+    }
+  };
+};
+
+var mm2 = new Guitar( 'Music Man', 'JPXIII 7 strings', 3100.00 );
+var ib2 = new Guitar( 'Ibanez', 'JEM777 7 Strings', 5000.00 );
+
+
+console.log( mm2 );
+console.log( ib2 );
+
+console.log( mm2.getNameAndPrice() );
+console.log( ib2.getNameAndPrice() );
+
+console.log( mm2.comparePrice( ib2 ) );
+console.log( ib2.comparePrice( mm2 ) );
+
+console.log( mm2.comparePrice( { brand: 'Jackson', model: 'Soloist', price: 1300.00 } ) );
