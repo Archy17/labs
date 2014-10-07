e9ced96a588071d3873af8ebd56bcb1db4f0e764
ericdouglas <eric.eoop@gmail.com>
2014-10-07 09:37:32 -0300

    JavaScript: unshift() method ~> Array

---
+++ javascript/Arrays/unshift.js
@@ -0,0 +1,20 @@
+/*
+
+  Array.unshift( valores )
+
+  - Insere argumentos no início do array
+  ps: retorna o tamanho do array
+
+*/
+
+var numbers = [ 4, 5, 6 ];
+
+numbers.unshift( 3 );
+console.log( numbers );
+
+console.log( numbers.unshift( 2 ) );
+console.log( numbers );
+
+
+console.log( numbers.unshift( 1 ) );
+console.log( numbers );
