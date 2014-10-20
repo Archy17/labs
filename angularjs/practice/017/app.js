eb1f648a4a4de589eabece0a0055b1860974a401
ericdouglas <eric.eoop@gmail.com>
2014-10-20 15:28:04 -0200

    AngularJS: Practice 17 - adding array in ngClass

--- angularjs/practice/017/app.js
+++ angularjs/practice/017/app.js
@@ -4,6 +4,7 @@ app.controller( 'ClassController', function( $scope ) {
 
     $scope.check = true;
     $scope.node = 'node';
+    $scope.array = [ 's2', 'big' ];
 
     $scope.change = function() {
       $scope.check = !$scope.check;
