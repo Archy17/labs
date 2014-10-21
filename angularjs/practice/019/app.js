7fa5323f7689df56f1c3723e2f3ba259ef5f24a2
ericdouglas <eric.eoop@gmail.com>
2014-10-21 06:19:14 -0200

    AngularJS: Practice 19

---
+++ angularjs/practice/019/app.js
@@ -0,0 +1,9 @@
+var app = angular.module( 'app', [] );
+
+app.controller( 'ModelController', function( $scope ) {
+  $scope.data = '';
+});
+
+app.controller( 'SubModelController', function( $scope ) {
+
+});
