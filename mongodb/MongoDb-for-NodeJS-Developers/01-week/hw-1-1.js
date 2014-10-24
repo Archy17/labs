94ae96b9f2234d53d05955e3c35f0e9661d5fe6d
ericdouglas <eric.eoop@gmail.com>
2014-10-24 20:32:37 -0200

    hw1-1

---
+++ mongodb/MongoDb-for-NodeJS-Developers/01-week/hw-1-1.js
@@ -0,0 +1,16 @@
+/* Homework 1-1 */
+
+1. mongo
+
+2. show dbs
+-> local
+-> m101
+
+3. use m101
+
+4. show collections
+-> hw1_1
+-> system.indexes
+
+5. db.hw1_1.find()
+-> { "_id" : ObjectId("51e4524ef3651c651a42331c"), "answer" : "Hello from MongoDB!" }
