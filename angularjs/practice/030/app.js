e7049cbca2990523e64307c87100452064fb7a15
ericdouglas <eric.eoop@gmail.com>
2014-10-27 06:33:19 -0200

    AngularJS: Practice 30

--- angularjs/practice/030/app.js
+++ angularjs/practice/030/app.js
@@ -7,13 +7,18 @@
 
     function ListController() {
       var vm = this;
+      vm.getClass = getClass;
       vm.data = [
         {label: 'First Note', done: false, assignee: 'Shyam'},
         {label: 'Second Note', done: false},
         {label: 'Done Note', done: true},
         {label: 'Last Note', done: false, assignee: 'Brad'}
       ];
-      vm.getClass = getClass;
+      vm.guitars = {
+        Ibanez   : { price : 1300 },
+        musicman : { price : 3100 },
+        Mayones  : { price : 3370 }
+      };
 
       function getClass( status ) {
         return status ? 'alert alert-success' : 'alert alert-warning';
