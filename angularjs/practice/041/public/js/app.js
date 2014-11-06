4b6c2719368b50a9855bd60ae04a570ed3c1eb1b
ericdouglas <eric.eoop@gmail.com>
2014-11-06 08:22:17 -0200

    AngularJS: Practice 41 - new app structure

--- angularjs/practice/041/public/js/app.js
+++ angularjs/practice/041/public/js/app.js
@@ -2,21 +2,6 @@
   'use strict';
 
   angular
-    .module( 'app', [] )
-    .controller( 'GetController', GetController );
-
-  GetController.$inject = [ '$http' ];
-
-  function GetController( $http ) {
-    var vm = this;
-    vm.items = [];
-    vm.title = 'Angular $http';
-
-    $http.get( '/api/note' ).then(function( response ) {
-      vm.items = response.data;
-    }, function( errResponse ) {
-      console.error( 'Error while fetching notes' );
-    });
-  }
+    .module( 'app', [] );
 
 }());
