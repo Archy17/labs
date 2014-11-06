a24db3e6f5304e82efae030e576a616ff927d0bf
ericdouglas <eric.eoop@gmail.com>
2014-11-06 10:14:59 -0200

    AngularJS: Practice 42 - upgrade

--- angularjs/practice/042/app.js
+++ angularjs/practice/042/app.js
@@ -8,26 +8,17 @@
   function AutoController() {
     var vm = this;
     vm.data = [
-      {
-        "brand" : "ibanez",
-        "price" : 1300
-      },
-      {
-        "brand" : "music man",
-        "price" : 3100
-      },
-      {
-        "brand" : "caparison",
-        "price" : 3300
-      },
-      {
-        "brand" : "PRS",
-        "price" : 4300
-      },
-      {
-        "brand" : "Jackson",
-        "price" : 1100
-      }
+      { "brand" : "MusicMan1", "price" : 1000 },
+      { "brand" : "MusicMan2", "price" : 2000 },
+      { "brand" : "MusicMan3", "price" : 3000 },
+      { "brand" : "MusicMan4", "price" : 4000 },
+      { "brand" : "MusicMan5", "price" : 5000 },
+      { "brand" : "MusicMan6", "price" : 6000 },
+      { "brand" : "MusicMan7", "price" : 7000 },
+      { "brand" : "MusicMan8", "price" : 8000 },
+      { "brand" : "MusicMan9", "price" : 9000 },
+      { "brand" : "MusicMan10", "price" : 10000 },
+      { "brand" : "MusicMan11", "price" : 11000 }
     ];
 
 
