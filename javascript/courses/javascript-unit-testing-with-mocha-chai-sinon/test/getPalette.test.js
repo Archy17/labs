4a8c88cf9d2da8743336e406855b70da7e7dddef
ericdouglas <eric.eoop@gmail.com>
2014-11-16 13:05:10 -0200

    1.4 - create a new test that will fail

--- javascript/courses/javascript-unit-testing-with-mocha-chai-sinon/test/getPalette.test.js
+++ javascript/courses/javascript-unit-testing-with-mocha-chai-sinon/test/getPalette.test.js
@@ -3,6 +3,12 @@ var assert = require( 'assert' );
 
 describe( 'getPalette', function() {
 
+  it( 'should throw an error if the result is not an array', function() {
+
+    assert.throws( getPalette, /is not an array/ );
+
+  });
+
   it( 'should return an array with 3 items', function() {
 
     var palette = getPalette();
