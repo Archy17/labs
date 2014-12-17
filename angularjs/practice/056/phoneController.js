8f5a7fa4778691a27adcdd077e269ac0b5009897
ericdouglas <eric.eoop@gmail.com>
2014-12-17 10:00:19 -0200

    Angularjs: practice 56

---
+++ angularjs/practice/056/phoneController.js
@@ -0,0 +1,24 @@
+(function() {
+  
+  'use strict';
+
+  angular
+    .module( 'app' )
+    .controller( 'PhoneController', PhoneController );
+
+  PhoneController.$inject = [];
+
+  function PhoneController() {
+
+    var vm = this;
+
+    ///////////// Properties
+    vm.phone = '';
+
+    ///////////// Methods
+
+    /////////////
+
+  }
+
+}());
---
+++ angularjs/practice/056/phoneDirective.js
@@ -0,0 +1,42 @@
+(function() {
+  
+  'use strict';
+
+  angular
+    .module( 'app' )
+    .directive( 'phoneValidator', phoneValidator );
+
+  phoneValidator.$inject = [];
+
+  function phoneValidator() {
+
+    return {
+      require: 'ngModel',
+      link: function( scope, element, attrs, modelCtrl ) {
+
+        modelCtrl.$parsers.push( function ( input ) {
+
+          var length = input.length;
+          var maxSize = 15;
+
+          if ( length > maxSize ) {
+            input = input.substring( length - maxSize, length );
+          }
+
+          var transformedInput = input
+                                    .replace( /\D/g, "" )
+                                    .replace( /^(\d{2})(\d)/g, "($1) $2" )
+                                    .replace( /(\d)(\d{4})$/,"$1-$2" );
+
+          if ( transformedInput != input ) {
+            modelCtrl.$setViewValue( transformedInput );
+            modelCtrl.$render();
+          }         
+
+          return transformedInput;         
+        });
+      }
+    };
+  }
+
+}());
