f4a750d62b52e29f9560fa1b3768f39ddc494e4b
ericdouglas <eric.eoop@gmail.com>
2014-12-20 10:35:21 -0200

    Chai -> 001 - fix sinon file to our html

---
+++ chai/practice/001/test/cow_sinon_test.js
@@ -0,0 +1,30 @@
+var expect = chai.expect;
+
+describe( 'Cow', function() {
+  var sandbox;
+
+  beforeEach( function() {
+    // create a sandbox
+    sandbox = sinon.sandbox.create();
+
+    // stub some console methods
+    sanbox.stub( window.console, 'log' );
+    sandbox.stub( window.console, 'error' );
+  });
+
+  afterEach( function() {
+    // restore the environment as it was before
+    sandbox.restore();
+  });
+
+  // ...
+  describe( '#logGreets', function() {
+    it( 'should log an error if no target is passed in', function() {
+      ( new Cow()).logGreets();
+
+      sinon.assert.notCalled( console.log );
+      sinon.assert.calledOnce( console.error );
+      sinon.assert.calledWithExactly( console.error, 'Missing target');
+    });
+  });
+});
