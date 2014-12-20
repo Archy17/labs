e8ae26107c017e161f4bb390adb150b2f0f438e0
ericdouglas <eric.eoop@gmail.com>
2014-12-20 08:23:42 -0200

    Chai -> 001 - testing constructor

---
+++ chai/practice/001/test/cow_test.js
@@ -0,0 +1,14 @@
+var expect = chai.expect;
+
+describe( 'Cow', function() {
+  describe( 'Constructor', function() {
+    it( 'should have a default name', function() {
+      var cow = new Cow();
+      expect( cow.name ).to.equal( 'Anon Cow' );
+    });
+    it( 'should set cow\'s name if provided', function() {
+      var cow = new Cow( 'Kate' );
+      expect( cow.name ).to.equal( 'Kate' );
+    });
+  });
+});
