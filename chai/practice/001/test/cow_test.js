f97ca0aaca6639fd8b788a88fee050eee5d437b4
ericdouglas <eric.eoop@gmail.com>
2014-12-20 08:53:29 -0200

    Chai -> 001 - test lateGreets function (async function)

--- chai/practice/001/test/cow_test.js
+++ chai/practice/001/test/cow_test.js
@@ -6,6 +6,7 @@ describe( 'Cow', function() {
       var cow = new Cow();
       expect( cow.name ).to.equal( 'Anon Cow' );
     });
+
     it( 'should set cow\'s name if provided', function() {
       var cow = new Cow( 'Kate' );
       expect( cow.name ).to.equal( 'Kate' );
@@ -18,9 +19,26 @@ describe( 'Cow', function() {
         ( new Cow() ).greets();
       }).to.throw( Error );
     });
+
     it( 'should greet passed target', function() {
       var greetings = ( new Cow( 'Kate' )).greets( 'Baby' );
-      expect( greetings ).to.equal( 'Kate greets Baby' );
+      expect( greetings ).to.equal( 'Kate greets Baby!' );
+    });
+  });
+  
+  describe( '#lateGreets', function() {
+    it( 'should pass an error if no target is passed', function( done ) {
+      ( new Cow()).lateGreets( null, function( err, greetings ) {
+        expect( err ).to.be.an.instanceof( Error );
+        done();
+      });
+    });
+
+    it( 'should greet passed target after one second', function( done ) {
+      ( new Cow( 'Kate' )).lateGreets( 'Baby', function( err, greetings ) {
+        expect( greetings ).to.equal( 'Kate greets Baby!' );
+        done();
+      });
     });
   });
 });
