d145c7902d97f2fcbbbda86a28cdcb5393ab152a
ericdouglas <eric.eoop@gmail.com>
2014-12-24 16:25:20 -0200

    nodejs -> 006 - use all files to show usability of path-to module

--- nodejs/practice/006/some/folder/here/app.js
+++ nodejs/practice/006/some/folder/here/app.js
@@ -2,4 +2,7 @@ var path = require( 'path-to' );
 var data = require( path( 3, 'config.json' ));
 var sum  = require( path( 3, 'sum' ) );
 
-console.log( sum( data.a + data.b ));
+console.log( data );
+console.log( sum( 10, 3 ));
+
+console.log( sum( data.a, data.b ));
