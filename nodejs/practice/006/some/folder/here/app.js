0cdeee56cc883a9045ec2b20cc44954046d8709c
ericdouglas <eric.eoop@gmail.com>
2014-12-24 16:32:49 -0200

    nodejs -> 006 - show the two possible ways to use path-to module

--- nodejs/practice/006/some/folder/here/app.js
+++ nodejs/practice/006/some/folder/here/app.js
@@ -1,6 +1,12 @@
 var path = require( 'path-to' );
+
+// or you could use just the "../../" abstraction
+// naming in the way you feel confortable
+var p3   = require( 'path-to' )( 3 );
+
+// using path-to module
 var data = require( path( 3, 'config.json' ));
-var sum  = require( path( 3, 'sum' ) );
+var sum  = require( p3 + 'sum' );
 
 console.log( data );
 console.log( sum( 10, 3 ));
