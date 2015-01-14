78cefc94470fdc73d1c40ed63c7bd69cd4b93ea6
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-14 15:32:10 -0200

    angularjs -> angular-multi-select add groups

--- angularjs/practice/057/controller.js
+++ angularjs/practice/057/controller.js
@@ -15,11 +15,57 @@
     ///////////// Properties
 
     vm.inputData = [
-      { name: "Opera",             maker: "(Opera Software)",     ticked: true  },
-      { name: "Internet Explorer", maker: "(Microsoft)",          ticked: false },
-      { name: "Firefox",           maker: "(Mozilla Foundation)", ticked: true  },
-      { name: "Safari",            maker: "(Apple)",              ticked: false },
-      { name: "Chrome",            maker: "(Google)",             ticked: true  }
+      { 
+        name: "<strong>All Browsers</strong>",
+        multiSelectGroup: true
+      },
+      { 
+        name: "<strong>Modern Browsers</strong>",
+        multiSelectGroup: true
+      },
+      { 
+        name: "Opera",
+        maker: "(Opera Software)",     
+        ticked: true  
+      },
+      { 
+        name: "Firefox",           
+        maker: "(Mozilla Foundation)", 
+        ticked: true  
+      },
+      { 
+        name: "Safari",           
+         maker: "(Apple)",              
+         ticked: false 
+      },
+      { 
+        name: "Chrome",            
+        maker: "(Google)",             
+        ticked: true  
+      },
+      {
+        multiSelectGroup: false
+      },
+      { 
+        name: "<strong>Classic Browsers</strong>",
+        multiSelectGroup: true
+      },
+      { 
+        name: "Internet Explorer", 
+        maker: "(Microsoft)",          
+        ticked: false 
+      },
+      { 
+        name: "NetScape Navigator",            
+        maker: "(NetScape Corporation)",             
+        ticked: true  
+      },
+      {
+          multiSelectGroup: false
+      },
+      {
+          multiSelectGroup: false
+      }
     ];
 
     vm.outputData = [];
