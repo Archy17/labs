d6a54b506a70cbe401fe15dcb18510c7ed14d214
ericdouglas <eric.douglas.mail@gmail.com>
2015-01-14 15:02:52 -0200

    angularjs -> angular-multi-select

---
+++ angularjs/practice/057/controller.js
@@ -0,0 +1,31 @@
+(function() {
+  
+  'use strict';
+
+  angular
+    .module( 'app' )
+    .controller( 'SelectController', SelectController );
+
+  SelectController.$inject = [];
+
+  function SelectController() {
+
+    var vm = this;
+
+    ///////////// Properties
+
+    vm.inputData = [
+      { name: "Opera",             maker: "(Opera Software)",     ticked: true  },
+      { name: "Internet Explorer", maker: "(Microsoft)",          ticked: false },
+      { name: "Firefox",           maker: "(Mozilla Foundation)", ticked: true  },
+      { name: "Safari",            maker: "(Apple)",              ticked: false },
+      { name: "Chrome",            maker: "(Google)",             ticked: true  }
+    ];
+
+    ///////////// Methods
+
+    /////////////
+
+  }
+
+}());
