d6a36afe504ed5cd3958e50f0b801ed09c0588b7
ericdouglas <eric.douglas.mail@gmail.com>
2015-03-30 04:54:25 -0300

    create serve methods to reuse code

---
+++ nodejs/hapijs/server-methods/server.js
@@ -0,0 +1,47 @@
+var Hapi = require( 'hapi' );
+
+var server = new Hapi.Server();
+
+server.connection({
+  port : 3000,
+  host : 'localhost'
+});
+
+// Methods
+function add( x, y, next ) {
+  // the next callback is used to return values
+  next( null, x + y );
+}
+
+function multiply( x, y, next ) {
+  next( null, x * y );
+}
+
+// Server methods
+server.method( 'add', add, {});
+server.method([{
+  name    : 'multiply',
+  method  : multiply,
+  options : {}
+}]);
+
+// Routes
+server.route({
+  method  : 'GET',
+  path    : '/',
+  handler : function( request, reply ) {
+    server.methods.add( 2, 3, reply );
+  }
+});
+
+server.route({
+  method  : 'GET',
+  path    : '/add/{num*2}',
+  handler : function( request, reply ) {
+    
+  }
+});
+
+server.start( function() {
+  console.log( 'Running at:', server.info.uri );
+});
