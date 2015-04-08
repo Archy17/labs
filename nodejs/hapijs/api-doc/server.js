1263e0fe47f4cd40cdc3d59b1549a8b4615ccb30
ericdouglas <eric.douglas.mail@gmail.com>
2015-04-08 06:08:14 -0300

    hapi.js -> server.app

---
+++ nodejs/hapijs/api-doc/server.js
@@ -0,0 +1,27 @@
+var hapi = require( 'hapi' );
+
+var server = new hapi.Server();
+
+server
+  .connection({
+    host : 'localhost',
+    port : Number( process.argv[ 2 ] || 3000 )
+  });
+
+server
+  .app
+  .key = 'value';
+
+server
+  .route({
+    path    : '/',
+    method  : 'GET',
+    handler : function( request, reply ) {
+      reply( server.app.key );
+    }
+  });
+
+server
+  .start( function() {
+    console.log( 'Server running at:', server.info.uri );
+  });
