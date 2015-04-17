ed02c0d199d365d7849bd2261ca15a06f2064ebe
ericdouglas <eric.douglas.mail@gmail.com>
2015-04-17 08:03:13 -0300

    angularjs -> setup karma

---
+++ angularjs/practice/060/controller.js
@@ -0,0 +1,25 @@
+(function() {
+  
+  'use strict';
+
+  angular
+    .module( 'app' )
+    .controller( 'DirController', DirController );
+
+  DirController.$inject = [];
+
+  function DirController() {
+
+    var vm = this;
+
+    ///////////// Properties
+
+    ///////////// Public Methods
+
+    ///////////// Private Methods
+
+    ///////////// Methods Declarations
+
+  }
+
+}());
