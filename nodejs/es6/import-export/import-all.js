148dbd8522676a1c7428873e1d9b926107ed3cfd
Eric Douglas <eric.douglas.mail@gmail.com>
2015-10-11 07:15:51 -0300

    Add destructuring export

--- nodejs/es6/import-export/import-all.js
+++ nodejs/es6/import-export/import-all.js
@@ -1,4 +1,8 @@
 import * as test from './export-all';
+import * as greet from './export-all-destructuring';
 
 console.log(test.prop);
 test.method();
+
+greet.sayHello('Eric');
+greet.sayBye('Douglas');
