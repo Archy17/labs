e4946b85a972104c5645ec7adefbb3661f0f281f
Eric Douglas <eric.douglas.mail@gmail.com>
2015-10-27 12:23:23 -0200

    Node.js -> core api - finish assert section

--- nodejs/core-api/assert.js
+++ nodejs/core-api/assert.js
@@ -29,7 +29,7 @@ describe('#Assert Core Module', function() {
   it('Should tests for deep equality - assert.deepEqual()', function() {
     assert.deepEqual({ hey: 'Eric' }, { hey: 'Eric' }, 'Objects are not deep equal');
     assert.deepEqual('13', 13, 'Strings are equal to numbers1!11!');
-    assert.deepEqual([13], [13], 'What happened here?');
+    assert.deepEqual([13, '12345'], ['13', 12345], 'What happened here?');
   });
 
   it('Should tests for deep inequality - assert.notDeepEqual()', function() {
@@ -42,5 +42,44 @@ describe('#Assert Core Module', function() {
 
   it('Should tests for strict inequality ( !== ) - assert.notStrictEqual()', function() {
     assert.notStrictEqual({ hey: 'Eric' }, { hey: 'Eric' }, 'Objects are not strict equal');
+  });
+
+  it('Should tests for deep equality - primitives values are compared with strict equality operator - assert.deepStrictEqual()', function() {
+    assert.deepStrictEqual([13, 12345], [13, 12345], 'Internal server error');
+  });
+
+  it('Should tests for deep inequality - primitives values are compared with strict inequality operator - assert.notDeepStrictEqual()', function() {
+    assert.notDeepStrictEqual([13, '12345'], ['13', 12345], 'Internal server error');
+  });
+
+  it('Should thrown an error - assert.throws()', function() {
+    assert.throws(function() {
+      throw new Error('Wrong value');
+    }, Error, 'Something bad ocurred');
+    
+    assert.throws(function() {
+      throw new Error('Wrong again...');
+    }, /again/, 'Something bad ocurred');
+
+    assert.throws(function() {
+      throw new Error('Wrong value');
+    }, function(err) {
+      if ((err instanceof Error) && /value/.test(err)) {
+        return true;
+      }
+    }, 'Something bad ocurred');
+  });
+
+  it('Should not thrown an error from another type - assert.doesNotThrow()', function() {
+    // will throw TypeError
+    // assert.doesNotThrow(function() {
+    //   throw new TypeError('Wrong value');
+    // }, SyntaxError, 'Weird...');
+  });
+
+  it('Should throws value if value is truthy', function() {
+    // useful when testing the error argument in callbacks
+    // assert.ifError('Some value');   
+    // Error: the string "Some value" was thrown, throw an Error :)
   });
 });
