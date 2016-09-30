c54f496c6519d674709e9a7555ead267938fb4bc
Eric Douglas <eric.douglas.mail@gmail.com>
2016-09-30 06:55:22 -0300

    Finish react tutorial

--- react/react-tutorial/public/scripts/example.js
+++ react/react-tutorial/public/scripts/example.js
@@ -1,8 +1,3 @@
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is **one comment**"},
-  {id: 2, author: "Jordan Walke", text: "This is *another comment*"}
-];
-
 var CommentBox = React.createClass({
   getInitialState: function() {
     return {
@@ -24,6 +19,32 @@ var CommentBox = React.createClass({
     });
   },
 
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+
+    comment.id = Date.now();
+
+    // .concat() doesn't modify the original array
+    var newComments = comments.concat([comment]);
+
+    // Optimistic updates
+    this.setState({ data: newComments });
+
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({ data: comments });
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
   componentDidMount: function() {
     this.loadCommentsFromServer();
     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
@@ -34,7 +55,7 @@ var CommentBox = React.createClass({
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={ this.state.data } />
-        <CommentForm />
+        <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
       </div>
     );
   }
@@ -58,11 +79,49 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {
+      author: '',
+      text: ''
+    };
+  },
+
+  handleAuthorChange: function(e) {
+    this.setState({ author: e.target.value });
+  },
+
+  handleTextChange: function(e) {
+    this.setState({ text: e.target.value });
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var author = this.state.author.trim();
+    var text   = this.state.text.trim();
+
+    if (!text || !author) {
+      return;
+    }
+
+    this.props.onCommentSubmit({ author: author, text: text });
+    this.setState({ author: '', text: '' });
+  },
+
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForma"
+            onSubmit={ this.handleSubmit }>
+        <input type="text"
+               placeholder="Your name"
+               value={ this.state.author }
+               onChange={ this.handleAuthorChange } />
+        <input type="text"
+               placeholder="Say something..."
+               value={ this.state.text }
+               onChange={ this.handleTextChange } />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
