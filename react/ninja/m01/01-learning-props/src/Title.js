a914edf4915fd3776e518637bf002e7fe584b4a4
Eric Douglas <eric.douglas.mail@gmail.com>
2016-12-13 08:31:18 -0200

    Add M1#A11 - pure functions

--- react/ninja/m01/01-learning-props/src/Title.js
+++ react/ninja/m01/01-learning-props/src/Title.js
@@ -1,23 +1,32 @@
 import React from 'react';
 
-const Title = React.createClass({
-  getDefaultProps() {
-    return {
-      name: 'Stranger',
-      lastName: 'without Last Name'
-    };
-  },
+const Title = ({ name, lastName }) => (
+  <h1>Hey, {`${name} ${lastName}`}</h1>
+);
 
-  render: function() {
-    return (
-      <div className="container">
-        <h1>Hey, {`${this.props.name} ${this.props.lastName}`}</h1>
+Title.defaultProps = {
+  name: 'Stranger',
+  lastName: 'without Last Name'
+};
 
-        <label htmlFor="input">Label htmlFor</label>
-        <input type="text" id="input"/>
-      </div>
-    );
-  }
-});
+// const Title = React.createClass({
+//   getDefaultProps() {
+//     return {
+//       name: 'Stranger',
+//       lastName: 'without Last Name'
+//     };
+//   },
+//
+//   render: function() {
+//     return (
+//       <div className="container">
+//         <h1>Hey, {`${this.props.name} ${this.props.lastName}`}</h1>
+//
+//         <label htmlFor="input">Label htmlFor</label>
+//         <input type="text" id="input"/>
+//       </div>
+//     );
+//   }
+// });
 
 export default Title;
