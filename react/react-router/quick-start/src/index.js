1d87db41b4c566bd9bc4f98b152b792dd2a1e2da
Eric Douglas <eric.douglas.mail@gmail.com>
2016-12-13 17:14:27 -0200

    Add 08-index-routes RRv3

--- react/react-router/quick-start/src/index.js
+++ react/react-router/quick-start/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import App from './app';
+import Home from './modules/Home';
 import About from './modules/About';
 import Repos from './modules/Repos';
 import Repo from './modules/Repo';
@@ -12,6 +13,7 @@ const renderApp = (NextApp) => {
     <AppContainer>
       <Router history={hashHistory}>
         <Route path="/" component={NextApp}>
+          <IndexRoute component={Home} />
           <Route path="/about" component={About} />
           <Route path="/repos" component={Repos}>
             <Route path="/repos/:userName/:repoName" component={Repo} />
