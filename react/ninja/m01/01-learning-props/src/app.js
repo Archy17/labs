f324b7559ea21e9dcb4ff03a98f025c66e9de2a3
Eric Douglas <eric.douglas.mail@gmail.com>
2016-12-13 08:48:29 -0200

    Add M01#A13 - key property

--- react/ninja/m01/01-learning-props/src/app.js
+++ react/ninja/m01/01-learning-props/src/app.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import Square from './Square';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Title name="Someone" />
+
+        {['red', 'green', 'blue'].map((color) => (
+          <Square
+            key={color}
+            color={color}
+          />
+        ))}
       </div>
     );
   }
