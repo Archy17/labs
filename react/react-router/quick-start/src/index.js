d317172cdf9c509a88ff2a7bf69856ca45e0f1a2
Eric Douglas <eric.douglas.mail@gmail.com>
2016-12-13 18:07:39 -0200

    Add 10-browserHistory RRv3

--- react/react-router/quick-start/src/index.js
+++ react/react-router/quick-start/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './app';
 import Home from './modules/Home';
 import About from './modules/About';
@@ -11,7 +11,7 @@ import Repo from './modules/Repo';
 const renderApp = (NextApp) => {
   render(
     <AppContainer>
-      <Router history={hashHistory}>
+      <Router history={browserHistory}>
         <Route path="/" component={NextApp}>
           <IndexRoute component={Home} />
           <Route path="/about" component={About} />
