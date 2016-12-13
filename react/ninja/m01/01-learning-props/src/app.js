a2ebcb4c098000b55332ccdcc988e6fd4ad0e984
Eric Douglas <eric.douglas.mail@gmail.com>
2016-12-13 08:03:48 -0200

    Add M1#A07 - passing props

---
+++ react/ninja/m01/01-learning-props/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Title from './Title';
+
+const App = React.createClass({
+  render: function() {
+    return (
+      <Title name="Eric Douglas" />
+    );
+  }
+});
+
+export default App;
