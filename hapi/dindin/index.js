cb73847ae758dc50934d690424a3e4e835b07a54
Eric Douglas <eric.douglas.mail@gmail.com>
2017-05-22 21:09:50 -0300

    Add simple endpoint GET recipes

---
+++ hapi/dindin/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Sqlite3 = require('sqlite3');
+
+const db = new Sqlite3.Database('./dindin.sqlite');
+const server = new Hapi.Server();
+
+server.connection({ port: 4000 });
+
+server.route([
+  {
+    method: 'GET',
+    path: '/api/recipes',
+    handler(request, reply) {
+      db.all('SELECT * FROM recipes', (err, results) => {
+        if (err) {
+          throw err;
+        }
+
+        reply(results);
+      });
+    },
+  }
+]);
+
+server.start(() => console.log(`Server listening at ${server.info.uri}`));
