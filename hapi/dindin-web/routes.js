8234d5dc9279bb6b68eab00e3e35500797e90e40
Eric Douglas <eric.douglas.mail@gmail.com>
2017-05-24 20:56:49 -0300

    Basic dynamic view

--- hapi/dindin-web/routes.js
+++ hapi/dindin-web/routes.js
@@ -1,3 +1,17 @@
 'use strict';
 
-module.exports = [];
+const Pages = require('./handlers/pages');
+const Assets = require('./handlers/assets');
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: Pages.home,
+  },
+  {
+    method: 'GET',
+    path: '/{param*}',
+    handler: Assets.servePublicDirectory,
+  }
+];
