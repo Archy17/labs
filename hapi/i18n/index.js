4c6a8bb51b19986006dda76f5f930b4560756718
Eric Douglas <eric.douglas.mail@gmail.com>
2017-06-17 21:04:56 -0300

    Add i18n custom handler

--- hapi/i18n/index.js
+++ hapi/i18n/index.js
@@ -1,8 +1,34 @@
 const Hapi = require('hapi');
 const Path = require('path');
+const Accept = require('accept');
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+  app: {
+    i18n: {
+      supportedLangs: ['en', 'fr', 'zh'],
+      defaultLang: 'en',
+    },
+  },
+});
 server.connection({ port: 4000 });
+
+server.handler('i18n-view', (route, options) => {
+  const view = options.view;
+
+  return function(request, reply) {
+    const settings = server.settings.app.i18n;
+    const langs = Accept.languages(request.headers['accept-language']);
+    const length = langs.length;
+
+    for (let i = 0; i < length; i += 1) {
+      if (settings.supportedLangs.indexOf(langs[i]) !== -1) {
+        return reply.view(`${view}_${langs[i]}`, options.context);
+      }
+    }
+
+    reply.view(`${view}_${settings.defaultLang}`);
+  };
+});
 
 server.register(require('vision'), err => {
   if (err) {
@@ -21,7 +47,10 @@ server.register(require('vision'), err => {
       method: 'GET',
       path: '/',
       handler: {
-        view: 'index',
+        'i18n-view': {
+          view: 'index',
+          context: { name: 'Eric' },
+        },
       },
     }
   ]);
