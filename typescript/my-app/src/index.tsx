1a5871ca4cac0b5485c6db5225faa47d007dd4bd
Eric Douglas <eric.douglas.mail@gmail.com>
2017-07-13 18:55:32 -0300

    Add typescript hello world

---
+++ typescript/my-app/src/index.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+import Hello from './containers/Hello';
+
+import './index.css';
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
