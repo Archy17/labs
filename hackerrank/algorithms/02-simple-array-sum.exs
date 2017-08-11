965a1e0820930e8462e6dac337f7e84741338cfa
Eric Douglas <eric.douglas.mail@gmail.com>
2017-08-11 09:03:50 -0300

    Add simple-array-sum resolution ex

---
+++ hackerrank/algorithms/02-simple-array-sum.exs
@@ -0,0 +1,42 @@
+"""
+Given an array of N integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, N, denoting the size of the array.
+The second line contains N space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+
+Sample Output
+
+31
+
+Explanation
+
+We print the sum of the array's elements, which is: 1 + 2 + 3 + 4 + 10 + 11 = 31.
+"""
+
+defmodule Solution do
+  defp sumIntegers(integers) do
+    integers
+    |> String.split
+    |> Enum.reduce(0, fn(num, acc) -> String.to_integer(num) + acc end)
+  end
+
+  def main do
+    length   = IO.gets("") |> String.trim # unused
+    integers = IO.gets("") |> String.trim
+
+    sumIntegers(integers) |> IO.puts
+  end
+end
+
+Solution.main()
