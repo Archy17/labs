2d117d7793c06bd91cf9eeacbb0b8edde0695370
Eric Douglas <eric.douglas.mail@gmail.com>
2017-09-02 08:33:57 -0300

    Add init and necessary functions - todo_csv.ex

--- elixir/general/lib/todo_csv/todo_csv.ex
+++ elixir/general/lib/todo_csv/todo_csv.ex
@@ -7,26 +7,42 @@ defmodule TodoCSV do
   end
 
   def read_file!(path) do
+    path
+    |> File.stream!
+    |> Stream.map(&String.replace(&1, "\n", ""))
+  end
 
+  def format_output(input) do
+    input
+    |> Stream.map(&String.split(&1, ","))
+    |> Stream.map(&format_row/1)
+  end
+
+  def format_row([date, task, id]) do
+    # Task: Name of the task
+    # Date: YYYY/MM/DD
+    # Task ID: 00
+    "Task: #{task}\nDate: #{date}\nTask ID: #{id}\n"
   end
 
   def show_all_todos(todo_list) do
-
+    todo_list
+    |> Enum.each(&IO.puts/1)
   end
 
-  def show_todo(todo_list, id) do
+  # def show_todo(todo_list, id) do
 
-  end
+  # end
 
-  def add_todo(todo_list, todo) do
+  # def add_todo(todo_list, todo) do
 
-  end
+  # end
 
-  def update_todo(todo_list, id, todo) do
+  # def update_todo(todo_list, id, todo) do
 
-  end
+  # end
 
-  def delete_todo(todo_list, id) do
+  # def delete_todo(todo_list, id) do
 
-  end
+  # end
 end
