d87b7686e39238272e49b5aeb118a27c3174bb99
Eric Douglas <eric.douglas.mail@gmail.com>
2017-09-05 19:26:11 -0300

    Add vue components practice

---
+++ vue/components-practice/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+
+    <hr>
+
+    <servers-list></servers-list>
+
+    <hr>
+
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppHeader from './AppHeader.vue'
+import ServersList from './ServersList.vue'
+import AppFooter from './AppFooter.vue'
+
+export default {
+  name: 'app',
+
+  components: {
+    'app-header': AppHeader,
+    'servers-list': ServersList,
+    'app-footer': AppFooter,
+  },
+}
+</script>
+
+<style>
+body {
+  background-color: #101010;
+  color: lime;
+}
+
+.list-group-item {
+  background-color: #303030;
+}
+</style>
---
+++ vue/components-practice/src/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <footer>
+        <p>
+          All servers are managed here
+        </p>
+      </footer>
+    </div>
+  </div>
+</template>
