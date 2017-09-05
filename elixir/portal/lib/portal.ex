d9906797af2427aa7880410318a7fcc4065c0d31
Eric Douglas <eric.douglas.mail@gmail.com>
2017-09-05 18:51:14 -0300

    Add elixir portal wip

---
+++ elixir/portal/lib/portal.ex
@@ -0,0 +1,8 @@
+defmodule Portal do
+  @doc """
+  Starts transfering `data` from `left` to `right`.
+  """
+  def transfer left, right, data do
+    # First add all data to the portal on the left
+  end
+end
