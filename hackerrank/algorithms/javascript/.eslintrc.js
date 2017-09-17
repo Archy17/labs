fbc43d26892c963cdfd508cfe42a91ae22e19055
Eric Douglas <eric.douglas.mail@gmail.com>
2017-09-17 20:31:05 -0300

    Add eslint config

---
+++ hackerrank/algorithms/javascript/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+    "plugin:jest/recommended"
+  ],
+  "plugins": [
+    "jest"
+  ],
+  "env": {
+    "jest/globals": true
+  },
+  "rules": {
+    semi: "off"
+  }
+};
