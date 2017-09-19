ed66e35f90efeb628a19db70c518a6ddc2b72024
Eric Douglas <eric.douglas.mail@gmail.com>
2017-09-19 11:27:58 -0300

    Refactor to pass all cases bigSorting js algorithm

--- hackerrank/algorithms/javascript/04-sorting/01-big-sorting.js
+++ hackerrank/algorithms/javascript/04-sorting/01-big-sorting.js
@@ -1,3 +1,4 @@
+/*
 function bigSorting(list) {
   return list.sort((a, b) => {
     const aSize = a.length
@@ -15,6 +16,28 @@ function bigSorting(list) {
 
     // a is equal to b
     return 0
+  })
+}
+*/
+
+function bigSorting(list) {
+  return list.sort((a, b) => {
+    const aSize = a.length
+    const bSize = b.length
+
+    if (a === b) return 0
+
+    if (aSize < bSize) return -1
+
+    if (aSize > bSize) return 1
+
+    for (let i = 0; i <= aSize; i += 1) {
+      const aChar = Number(a[i])
+      const bChar = Number(b[i])
+
+      if (aChar < bChar) return -1
+      if (aChar > bChar) return 1
+    }
   })
 }
 
