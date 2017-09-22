04ccbd083e79853d442d146ec2d989416ef03c7b
Eric Douglas <eric.douglas.mail@gmail.com>
2017-09-22 08:08:10 -0300

    Remove heavy rust files

--- rust/rust_book_v2/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/ericdouglas/github/labs/rust/rust_book_v2/guessing_game/target/debug/guessing_game: /home/ericdouglas/github/labs/rust/rust_book_v2/guessing_game/src/main.rs
--- rust/rust_book_v2/guessing_game/target/debug/libguessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/ericdouglas/github/labs/rust/rust_book_v2/guessing_game/target/debug/libguessing_game.rmeta: /home/ericdouglas/github/labs/rust/rust_book_v2/guessing_game/src/main.rs
