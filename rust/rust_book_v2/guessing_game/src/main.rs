0218ab78caa3115c17469def4b25c93986f1a807
Eric Douglas <eric.douglas.mail@gmail.com>
2017-09-25 06:04:36 -0300

    Guess number - rust

--- rust/rust_book_v2/guessing_game/src/main.rs
+++ rust/rust_book_v2/guessing_game/src/main.rs
@@ -1,14 +1,22 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
-  println!("Guess the number!");
-  println!("Please input your guess.");
+    println!("Guess the number!");
 
-  let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-  io::stdin()
-      .read_line(&mut guess)
-      .expect("Failed to read line");
+    println!("The secret number is: {}", secret_number);
 
-  println!("You guessed: {}", guess)
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess)
 }
